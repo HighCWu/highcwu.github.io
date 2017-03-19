dfe6ce078add3a0e30853d4fe6919d85d6b94ce3
wuhecong <wuhecong@163.com>
2017-03-19 10:52:07 +0800

    UP

--- ncneed/ncindex.js
+++ ncneed/ncindex.js
@@ -1,6 +1,10 @@
 document.getElementById('mainFrame').onload=function(){loadFrame(this);};
 function loadFrame(obj){
 	var url = obj.contentWindow.location.href;
+	obj.onload=function(){};
+	obj.width = document.body.clientWidth;
+	iframe.focus();
+    clearInterval(resizeID);
 	obj.style.overflow="scroll";
 	document.getElementsByClassName('header')[0].style.overflow="scroll";
 	if(url.indexOf("ViewTestTask.aspx")!=-1){
