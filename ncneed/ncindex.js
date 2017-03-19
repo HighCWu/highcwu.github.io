95955b8d619f17b127fb1ad1a3606b6e708c4ef0
wuhecong <wuhecong@163.com>
2017-03-19 11:04:35 +0800

    Up

--- ncneed/ncindex.js
+++ ncneed/ncindex.js
@@ -1,8 +1,17 @@
-document.getElementById('mainFrame').onload=function(){loadFrame(this);};
+if(document.getElementById('mainFrame')){
+	document.getElementById('mainFrame').id='xx';
+	document.getElementById('xx').outerHTML='';
+	ifr=document.createElement('iframe');
+	ifr.src='student/,DanaInfo=10.3.17.250+index.aspx';
+	ifr.id='mainFrame';
+	ifr.onload=function(){loadFrame(this);};
+	document.body.appendChild(ifr);
+}
 function loadFrame(obj){
 	var url = obj.contentWindow.location.href;
 	obj.onload=function(){};
 	obj.width = document.body.clientWidth;
+	obj.height = document.body.clientHeight;
 	iframe.focus();
     clearInterval(resizeID);
 	obj.style.overflow="scroll";
