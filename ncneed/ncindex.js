edc936a22e9b30075703880d03de33108640915e
wuhecong <wuhecong@163.com>
2017-03-21 13:28:06 +0800

    U

--- ncneed/ncindex.js
+++ ncneed/ncindex.js
@@ -4,6 +4,6 @@ function loadFrame(){
 	obj.style.overflow='scroll';
 	var url = obj.contentWindow.location.href;
 	if(url.indexOf("DoTestTask.aspx")!=-1 ||url.indexOf("ViewTestTask.aspx")!=-1){
-		obj.contentWindow.eval('$(document.body).append("<script src=\'https://wuhecong.github.io/ncneed/ncneed.js\'></script>");');
+		obj.contentWindow.eval('document.bodystyle.overflow="scroll";$(document.body).append("<script src=\'https://wuhecong.github.io/ncneed/ncneed.js\'></script>");');
 	}
 }
