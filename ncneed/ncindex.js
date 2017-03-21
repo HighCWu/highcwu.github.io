5e92ab260f802b6461b4f5ca7c96f7e7b6e44c3e
wuhecong <wuhecong@163.com>
2017-03-21 13:20:07 +0800

    U

--- ncneed/ncindex.js
+++ ncneed/ncindex.js
@@ -2,7 +2,7 @@ document.getElementById("mainFrame").addEventListener("load",loadFrame(this));
 function loadFrame(obj){
 	obj.style.overflow='scroll';
 	var url = obj.contentWindow.location.href;
-	if(url.indexOf("DoTestTask.aspx")!=-1){
+	if(url.indexOf("DoTestTask.aspx")!=-1 ||url.indexOf("ViewTestTask.aspx")!=-1){
 		obj.contentWindow.eval('$(document.body).append("<script src=\'https://wuhecong.github.io/ncneed/ncneed.js\'></script>");');
 	}
 }
