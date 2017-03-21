ab4eb74a4b91c5a77a522ea78c065c1cee4262d5
wuhecong <wuhecong@163.com>
2017-03-21 13:37:37 +0800

    U

--- ncneed/ncindex.js
+++ ncneed/ncindex.js
@@ -2,10 +2,10 @@ var obj = document.getElementById("mainFrame");
 obj.addEventListener("load",loadFrame());
 obj.style.overflow='scroll';
 var url = obj.contentWindow.location.href;
+obj.contentWindow.eval('document.bodystyle.overflow="scroll";');
 console.log(url);
 if(url.indexOf("DoTestTask.aspx")!=-1 ||url.indexOf("ViewTestTask.aspx")!=-1){
-	obj.contentWindow.eval('document.bodystyle.overflow="scroll";$(document.body).append("<script src=\'https://wuhecong.github.io/ncneed/ncneed.js\'></script>");');
-	console.log('确实注入了');
+console.log('确实注入了');
 }
 function loadFrame(){
 	obj.style.overflow='scroll';
