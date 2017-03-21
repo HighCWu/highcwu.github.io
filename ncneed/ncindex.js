81ad241bd63f84ef0a3214bf150727fe7b1356b8
wuhecong <wuhecong@163.com>
2017-03-21 13:13:57 +0800

    U

--- ncneed/ncindex.js
+++ ncneed/ncindex.js
@@ -1,7 +1,8 @@
-document.getElementById('mainFrame').onload=function(){loadFrame(this);};
+document.getElementById("mainFrame").addEventListener("load",loadFrame(this));
 function loadFrame(obj){
+	obj.style.overflow='scroll';
 	var url = obj.contentWindow.location.href;
-	if(url.indexOf("ViewTestTask.aspx")!=-1){
+	if(url.indexOf("DoTestTask.aspx")!=-1){
 		obj.contentWindow.eval('$(document.body).append("<script src=\'https://wuhecong.github.io/ncneed/ncneed.js\'></script>");');
 	}
 }
