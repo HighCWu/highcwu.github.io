03f12adb654bdce30dd100d54701ccae3d5c8346
wuhecong <wuhecong@163.com>
2017-03-22 14:14:51 +0800

    U

--- ncneed/ncindex.js
+++ ncneed/ncindex.js
@@ -1,6 +1,6 @@
 function loadFrame(){
 	console.log('mamade');
-	obj.contentWindow.eval('document.body.style.overflow="scroll";if(window.location.href.indexOf("DoTestTask.aspx")!=-1 || window.location.href.indexOf("ViewTestTask.aspx")!=-1){console.log(window.location.href);$(document.body).append("<script src=\'https://wuhecong.github.io/ncneed/ncneed.js\'></script>");console.log("确实注入了");}');
+	obj.contentWindow.eval('$(document.body).append("<link href=\'https://wuhecong.github.io/ncneed/common.css\' rel=\'stylesheet\' type=\'text/css\' />");document.body.style.overflow="scroll";if(window.location.href.indexOf("DoTestTask.aspx")!=-1 || window.location.href.indexOf("ViewTestTask.aspx")!=-1){console.log(window.location.href);$(document.body).append("<script src=\'https://wuhecong.github.io/ncneed/ncneed.js\'></script>");console.log("确实注入了");}');
 }
 var obj = document.getElementById("mainFrame");
 obj.addEventListener("load",loadFrame);
