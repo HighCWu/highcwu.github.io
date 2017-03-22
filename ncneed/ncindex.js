d08f1607efea2b61c938c503318f1f8e2d4ce330
wuhecong <wuhecong@163.com>
2017-03-22 14:11:11 +0800

    UP

--- ncneed/ncindex.js
+++ ncneed/ncindex.js
@@ -1,16 +1,8 @@
-var obj = document.getElementById("mainFrame");
-obj.addEventListener("load",loadFrame());
-var url = obj.contentWindow.location.href;
-obj.contentWindow.eval('document.body.style.overflow="scroll";');
-console.log(url);
-if(url.indexOf("DoTestTask.aspx")!=-1 ||url.indexOf("ViewTestTask.aspx")!=-1){
-console.log('确实注入了');
-}
 function loadFrame(){
-	url = obj.contentWindow.location.href;
-	console.log('in '+ url);
-	if(url.indexOf("DoTestTask.aspx")!=-1 || url.indexOf("ViewTestTask.aspx")!=-1){
-		obj.contentWindow.eval('document.body.style.overflow="scroll";$(document.body).append("<script src=\'https://wuhecong.github.io/ncneed/ncneed.js\'></script>");');
-		console.log('确实注入了');
-	}
+	console.log('mamade');
+	obj.contentWindow.eval('document.body.style.overflow="scroll";if(window.location.href.indexOf("DoTestTask.aspx")!=-1 || window.location.href.indexOf("ViewTestTask.aspx")!=-1){console.log(window.location.href);$(document.body).append("<script src=\'https://wuhecong.github.io/ncneed/ncneed.js\'></script>");console.log("确实注入了");}');
 }
+var obj = document.getElementById("mainFrame");
+obj.addEventListener("load",loadFrame);
+obj.contentWindow.eval('console.log(window.location.href);document.body.style.overflow="scroll";console.log("确实注入了");$(document.body).append("<link href=\'https://wuhecong.github.io/ncneed/common.css\' rel=\'stylesheet\' type=\'text/css\' />");');
+
